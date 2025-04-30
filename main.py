81a2851cca85f60e712ff692310b1e5f19d91cb5
Mafiaproo <76587921+Mafiaproo@users.noreply.github.com>
2025-04-30 13:43:17 +0200

    Chagement main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 import threading
 import time
+import winsound
 
 from traiter_m3u8 import download_movie
 from get_playlist_url_from_web import extract_m3u8_from_nested_iframe
@@ -10,6 +12,7 @@ root = tk.Tk()
 root.title("Legal movie downloader")
 root.iconbitmap("icon.ico")
 root.geometry("1000x600")
+root.resizable(width=False, height=False)
 
 # Contrôle
 stop_download = False
@@ -33,9 +36,14 @@ def search_and_download_movie():
     playlist_url = ""
     if "https://" in movie_input.get():
         playlist_url = extract_m3u8_from_nested_iframe(movie_input.get())
+    elif movie_input.get() == None or movie_input.get() == "":
+        log_to_textbox("You need to fill in the movie title or the url")
+        messagebox.showwarning("Movie entry null", "You need to fill in the movie title or the url for download the movie")
+        return
     else:
         log_to_textbox("❌ Fonction de recherche non encore implémentée.")
-        return
+        
+
 
     if not playlist_url:
         log_to_textbox("❌ Aucun lien m3u8 détecté.")
@@ -50,6 +58,7 @@ def search_and_download_movie():
             progress_callback=update_progress,
             stop_flag=lambda: stop_download
         )
+        messagebox.showinfo("Movie Download", "The movie has been successfully downloaded")
 
     download_thread = threading.Thread(target=task)
     download_thread.start()
@@ -59,6 +68,10 @@ def cancel_download():
     stop_download = True
     log_to_textbox("🛑 Téléchargement annulé par l'utilisateur.")
 
+def multiple_movie_message_box():
+    #messagebox.showinfo(title="Multiple Movies Founds", message="We found multiple movie with the keyword : " + movie_input.get())
+    messagebox.showerror(title="No Mobie found", message="We didnt found movie with the keyword : " + movie_input.get())
+
 # UI
 tk.Label(root, text="Legal movie downloader", font=("Arial", 30)).pack(pady=10)
 
@@ -67,6 +80,9 @@ movie_input.pack(pady=10)
 
 ttk.Button(root, text="Download Movie", command=search_and_download_movie).pack(pady=5)
 ttk.Button(root, text="Annuler", command=cancel_download).pack(pady=5)
+
+ttk.Button(root, text="Message box", command=multiple_movie_message_box).pack(pady=5)
+
 
 log_text_box = tk.Text(root, height=15, width=120, state="disabled")
 log_text_box.pack(pady=10)
